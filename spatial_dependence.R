6f77fc84a6d914d7013133afbd3130020ea6adde
Gloria Buriticá <gloriaburit@gmail.com>
2021-11-23 18:28:00 +0100

    Update spatial_dependence.R

--- spatial_dependence.R
+++ spatial_dependence.R
@@ -29,13 +29,45 @@ for( i in 1:3){
       plot(sample[   sapply(1:n, function(k) max(sample[k,c(a,b)])) > sample2[floor(n*0.94)] ,c(a,b)] ,col = "black", cex = 0.5, 
            pch=16,ylab=names2[(i-1)*3 + b] , xlab=names2[(i-1)*3 +a],
            main = names[i],
-           ylim = c(0,max(sample[,c(a,b)]) ) ,
-           xlim = c(0,max(sample[,c(a,b)]) )  )
+           ylim = c(0,max(sample[,1:3]) ) ,
+           xlim = c(0,max(sample[,1:3]) )  )
       points(sample[   sapply(1:n, function(k) max(sample[k,c(a,b)])) <= sample2[floor(n*0.94)] ,c(a,b)] ,col = "lightblue", cex = 0.5, 
              pch=16,ylab=names2[(i-1)*3 + b] , xlab=names2[(i-1)*3 +a],
              main = names[i])
       segments( sample2[floor(n*0.94)], sample2[floor(n*0.94)],0,sample2[floor(n*0.94)], col="lightblue" )
       segments( sample2[floor(n*0.94)],0, sample2[floor(n*0.94)],sample2[floor(n*0.94)] , col="lightblue")
+    }
+  }
+}
+
+
+
+
+par(mfrow=c(1,3))
+names2 <- c("BREST", "LANVEOC", "QUIMPER", "BORMES", "LE LUC","HYERES", "NANCY", "METZ", "ROVILLE")
+names  <- c("North West", "South", "North East")
+for( i in 2){
+  j <- 3
+  sample     <- cbind( pre[index[,j],((i-1)*3+2)] , pre[index[,j],((i-1)*3+3)] , pre[index[,j],((i-1)*3+4)] ) 
+  #sample     <- mARMAX1(al=4,par0=c(0.7,0.8,0.8),n0=n,dep0=dep[i],d0=3)
+  sample     <- na.omit(sample); n          <- length(sample[,1] )
+  sample2     <- sort(sapply(1:n, function(k) min(sample[k,]) ))
+  for( a in 1:2){
+    for(b in 2:3) if(a!= b) {
+      plot(sample[   sapply(1:n, function(k) min(sample[k,c(a,b)])) > sample2[floor(n*0.95)] ,c(a,b)] ,col = "black", cex = 0.5, 
+           pch=16,ylab=names2[(i-1)*3 + b] , xlab=names2[(i-1)*3 +a],
+           main = names[i],
+           ylim = c(0,sort(sample[,1:3],decreasing=T)[2] ) ,
+           xlim = c(0,sort(sample[,1:3],decreasing=T)[2] )  )
+      points(sample[   sapply(1:n, function(k) min(sample[k,c(a,b)])) <= sample2[floor(n*0.95)] ,c(a,b)] ,col = "lightblue", cex = 0.5, 
+             pch=16,ylab=names2[(i-1)*3 + b] , xlab=names2[(i-1)*3 +a],
+             main = names[i])
+      segments(sample2[floor(n*0.95)], sample2[floor(n*0.95)], sort(sample[,1:3],decreasing=T)[2], sample2[floor(n*0.95)], col="lightblue" )
+      segments(sample2[floor(n*0.95)], 0, sample2[floor(n*0.95)], sample2[floor(n*0.95)], col="lightblue" , lty=2)
+      
+      segments(sample2[floor(n*0.95)], sample2[floor(n*0.95)], sample2[floor(n*0.95)],sort(sample[,1:3],decreasing=T)[2] , col="lightblue" )
+      segments(0,sample2[floor(n*0.95)], sample2[floor(n*0.95)],sample2[floor(n*0.95)], col="lightblue" ,lty=2)
+      
     }
   }
 }
