e853d6d964f9fbfe68ebf9aa4bab6c9baa315e1d
Gloria Buriticá <gloriaburit@gmail.com>
2022-04-22 09:37:33 +0200

    Update extreme_quantiles.R

--- extreme_quantiles.R
+++ extreme_quantiles.R
@@ -6,8 +6,7 @@ library(latex2exp)
 
 
 par(mfrow=c(3,3))
-bu0<- c(165,105,70)
-kp0 <- c(350,250,350)
+
 ville <- c("Brest", "Lanveoc", "Quimper", "Bormes", "Le Luc", "Hyeres", "Nancy", "Metz", "Roville")
 region <- c("northwest", "south", "northeast")
 for(i in 1:3){
@@ -74,15 +73,18 @@ for(i in 1:3){
   }
 }
 
-par(mfrow=c(3,3))
-ylim0 <- xlim0 <- c(38,88)
+par(mfrow=c(1,3))
+ylim0 <- xlim0 <- c(44,140)
+MULT <- F
+bu0<- c(165,135,70)
+kp0 <- c(350,150,350)
 for(i in 3){
-  
+  #i <- 1
   bu<- bu0[i]
   kp<- kp0[i]
   j <- 3
   #sample0  <-  mARMAX1(al=4,par0=c(0.7,0.8,0.8),n0=n,dep0=0.9,d0=3)
-  
+
   sample0     <- na.omit(cbind( pre[index[,j],((i-1)*3+2)] , pre[index[,j],((i-1)*3+3)] , pre[index[,j],((i-1)*3+4)] ) )
   sample0     <- na.omit(sample0); n      <- length(sample0[,1] )
   sample     <- sapply(1:n, function(k) max(sample0[k, ]) )
@@ -92,13 +94,20 @@ for(i in 3){
   sumaalpha  <- sapply(1:floor(n/bu), function(k) sum(sample[((k-1)*bu + 1):(k*bu) ]^alpha) )
   ymax       <- length(sumaalpha)
   #mat        <- stableconfintervals(sumaalpha,alpha,ymax*1/min(moments))
+  bb               <- stable.fit.mle.restricted(sumaalpha, c(1,1,0,0), c(1,1,0,0))
   
   
-  ind        <- which(sample > records[floor(n*0.05) ] )
-  moments    <- sapply(1:3, function(d) mean( (sample0[ind,d]/sample[ind])^alpha ) )
+  ind        <- which(sample > records[floor( n*0.05) ] )
+  moments    <- sapply(1:3, function(d) mean( (sample0[ind,d]/sample[ind] )^alpha ) )
   print(moments)
   
-  for(ind in 1:3){
+  nmax       <- floor(0.02*n)
+  mat0      <-   stableconfintervals2(sumaalpha,alpha,moments)
+  
+  #mat        <- stableconfintervals(sumaalpha,alpha,moments)
+  
+  
+  if(MULT){ for(ind in 1:3){
     ## Creates sample
     sample     <- na.omit(cbind( pre[index[,j],((i-1)*3+2)] , pre[index[,j],((i-1)*3+3)] , pre[index[,j],((i-1)*3+4)] ) )
     sample     <- na.omit(sample); n      <- length(sample[,1] )
@@ -150,14 +159,51 @@ for(i in 3){
     
     abline(0,1,lty=2, col = "grey")
     mtext(ville[(i-1)*3 + ind], cex=0.8)
+  }}
+  else{ 
+    for(ind in 1:3){
+    
+    #sample     <- na.omit( pre[index[,j],((i-1)*3+ind+1)] )
+    #sample     <- na.omit(sample)
+    sample <- sample0[,ind]
+    quantiles4 <-  rev((sort(sample)))[nmax:1] 
+    
+    plot(NA,NA,pch=1,cex=1, xlab = "return levels",ylab= " " , #TeX('$1/\\widehat{\\alpha}^n$ - stable quantiles') , 
+         main = region[i], ylim =ylim0,xlim = log(xlim0) ,xaxt="n", yaxt="n", frame=F)
+    qb   <-  sapply( 1:nmax , function(k) qstable( ( ( 1-  k/(moments[ind]*n) )^bn ) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] )^(1/alpha)) ## quantiles
+
+    qb2   <- sapply( c(1,2,5,10,20,50,100,200), function(k) qstable( ( ( 1-  4/(moments[ind]*365*k) )^bn ) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] )^(1/alpha)) ## quantiles
+    
+    segments( log(qb2[6]), 0, log(qb2[6]) ,qb2[ 6] , col = "grey" , lty=2)
+    segments( log(qb2[6]), qb2[6], log(xlim0[2]), qb2[ 6], col="grey" , lty=2)
+    
+    #abline(0,1,lty=1, col = "grey")
+    lines( log(seq(xlim0[1], xlim0[2], by =1) ) , seq(xlim0[1], xlim0[2], by =1)   )
+    
+    points(log( rev(qb)) , quantiles4 )
+    lines( log(mat0[ ((ind-1)*8+ 1:8),2]), mat0[((ind-1)*8+ 1:8),1], lty=2)
+    lines( log(mat0[((ind-1)*8+ 1:8),2]),  mat0[((ind-1)*8+ 1:8),3], lty=2)
+    
+    
+    #lines( mat[1:nmax,2], mat[1:nmax,3] , lty = 2)
+    #lines( mat[1:nmax,2], mat[1:nmax,1] , lty = 2)
+    #abline(0,1)
+    #lines( quantiles4, mat[1:nmax,1], lty=2 )
+    #lines( quantiles4, mat[1:nmax,3], lty=2 )
+    #qb   <- sapply( c(5,10,20,50,100,200), function(Ti) qstable( rev( ( 1-  4/(moments[ind]*365*Ti) )^bn ) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] )^(1/alpha)) ## quantiles
+    axis(1, at=log(qb2),labels=c("1 yr","2 yr","5 yr","10 yr","20 yr", "50 yr", "100 yr ","200 yr"),
+         col.axis="black", las=2, cex.axis=0.9 ,tck=-.01,srt = 45)
+    axis(4, 
+         col.axis="darkgrey", las=1, cex.axis=0.8, tck=-.02,lwd = 0.5)
+   
+    mtext(ville[(i-1)*3 + ind], cex=0.8) 
+  } 
   }
-  
-  
 }
-ylim0 <- xlim0 <- c(73,180)
-ylim0 <- xlim0 <- c(25,70)
+ylim0 <- xlim0 <- c(90,270)
+ylim0 <- xlim0 <- c(35,115)
 
-
+ylim0 <- xlim0 <- c(4,15)
 
 
 
@@ -245,22 +291,25 @@ for(i in 1){
 
 
 
+nmax <- floor(0.02*n)
 
 stableconfintervals <- function(sumaalpha,alpha,mom=1){
   
   n2               <- length(sumaalpha)
   bb               <- stable.fit.mle.restricted(sumaalpha, c(1,1,0,0), c(1,1,0,0))
-  qb               <- qstable( rev(1- (((1:floor(mom*(n/bu)) ))/((n/bu)*mom))) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] ) ## quantiles
-  qb               <- sign(qb)*abs(qb)^(1/alpha)
+  qb               <- sapply( mom, function(m) qstable( rev( ( 1-  1:nmax/(m*n) )^bn ) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] )) ## quantiles
+  qb               <- sapply( 1:length(mom), function(m) sign(qb[,m] )*abs(qb[,m] )^(1/alpha))
   
   ran.gen.stable <- function(data, param) rstable( length(data), alpha=param$al,beta=1,gamma=param$gam,delta=param$del)
   ### Statistic
   statisticQ     <- function(data){
     d   <-  data 
     b   <-  stable.fit.mle.restricted(d, c(0,1,0,0), c(0,1,0,0))  ## fits a stable distribution with beta = 1
-    res <-  qstable( rev(1- (((1:floor(mom*(n/bu))))/((n/bu)*mom))),  alpha=  b[1], beta= b[2], 
-                               gamma = b[3], delta=b[4])
-    res <-  sign(res)*abs(res)^(1/alpha)
+    res1 <-  sapply( mom, function(m)
+               qstable( rev( ( 1-  1:nmax/(m*n) )^bn ),  alpha=  b[1], beta= b[2], 
+                               gamma = b[3], delta=b[4])  )
+    res <- NULL 
+    for( m in 1:length(mom) ) res <- cbind( res, sign(res1[,m])*abs(res1[,m])^(1/alpha) )
     return(res) 
   }
   ### Bootstrap iterates
@@ -268,10 +317,49 @@ stableconfintervals <- function(sumaalpha,alpha,mom=1){
               mle = list(al = bb[1],gam=bb[3],del=bb[4]) )
   ## CI
   mat <- NULL
-  for( j in 1:(n2*mom)){
-    b1  <- boot.ci(b, alpha = 0.05, type = "perc", index = j)
-    mat <- rbind( mat, c(b1$percent[4],qb[j],b1$percent[5]) )
+  for(l in 1:length(mom)){
+    for( j in 1:nmax ){
+      b1  <- boot.ci(b, alpha = 0.05, type = "perc", index = ((l-1)*nmax+j) )
+      mat <- rbind( mat, c(b1$percent[4],qb[j,l],b1$percent[5]) )
+    }
   }
+
+  
+  return(mat)
+}
+
+stableconfintervals2 <- function(sumaalpha,alpha,mom=1){
+  
+  n2               <- length(sumaalpha)
+  bb               <- stable.fit.mle.restricted(sumaalpha, c(1,1,0,0), c(1,1,0,0))
+  qb               <- sapply( mom, function(m) qstable( rev( ( 1-  4/(m*365*c(1,2,5,10,20,50,100,200) ) )^bn ) , alpha=bb[1], beta=bb[2], gamma=bb[3], delta=bb[4] )) ## quantiles
+  qb               <- sapply( 1:length(mom), function(m) sign(qb[,m] )*abs(qb[,m] )^(1/alpha))
+  
+  ran.gen.stable <- function(data, param) rstable( length(data), alpha=param$al,beta=1,gamma=param$gam,delta=param$del)
+  ### Statistic
+  statisticQ     <- function(data){
+    d   <-  data 
+    b   <-  stable.fit.mle.restricted(d, c(0,1,0,0), c(0,1,0,0))  ## fits a stable distribution with beta = 1
+    res1 <-  sapply( mom, function(m)
+      qstable( rev( ( 1-  4/(m*365*c(1,2,5,10,20,50,100,200)) )^bn ),  alpha=  b[1], beta= b[2], 
+               gamma = b[3], delta=b[4])  )
+    res <- NULL 
+    for( m in 1:length(mom) ) res <- cbind( res, sign(res1[,m])*abs(res1[,m])^(1/alpha) )
+    return(res) 
+  }
+  ### Bootstrap iterates
+  b   <- boot(sumaalpha, statistic = statisticQ, R = 1000, sim="parametric",ran.gen=ran.gen.stable,
+              mle = list(al = bb[1],gam=bb[3],del=bb[4]) )
+  ## CI
+  mat <- NULL
+  for(l in 1:length(mom)){
+    for( j in 1:8 ){
+      b1  <- boot.ci(b, alpha = 0.05, type = "perc", index = ((l-1)*8+j) )
+      mat <- rbind( mat, c(b1$percent[4],qb[j,l],b1$percent[5]) )
+    }
+  }
+  
+  
   return(mat)
 }
 ##### Computes index of spatial clustering
